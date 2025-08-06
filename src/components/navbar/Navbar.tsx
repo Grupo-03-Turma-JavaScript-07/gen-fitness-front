6ee731c170b276b07d4bbe8fd4674e620661bc55
Rodrigo Aguillar <flavioaguilar2001@gmail.com>
2025-08-06 13:48:07 -0300

    fix: Tirando o link da navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,7 @@
-import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
     <>
-      <div className="bg-slate-600">
-        Navbar
-        <Link to='/planos'> Planos</Link>
-      </div>
     </>
   )
 }
