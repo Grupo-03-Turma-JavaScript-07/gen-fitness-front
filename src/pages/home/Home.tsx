9a2e1a4be7353bc40c9b2cc41d6dabcaa25c1ca1
Samuel de Souza <samuelsouzarosajr@gmail.com>
2025-08-06 12:52:44 -0300

    feat: flipcard

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,25 @@
 
 function Home() {
+
   return (
     <>
-      <div className="bg-amber-700">
-        Home
+      <div className="bg-black">
+        <div className="container mx-auto p-6 ">
+          {/* Flipcard */}
+          <div className="group h-70 w-50 [perspective:1000px]">
+            <div className="card-effect">
+              {/* Front */}
+              <div className="flipcard-front">
+                <h2>Agilidade</h2>
+              </div>
+              {/* Back */}
+              <div className="flipcard-back">
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, beatae?</h2>
+              </div>  
+            </div>
+          </div>
+          {/* Fim flipcard */}
+        </div>
       </div>
     </>
   )
