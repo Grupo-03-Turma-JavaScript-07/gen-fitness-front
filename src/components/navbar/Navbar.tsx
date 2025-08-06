7409130e8a8d81e0aadcb69f69ecaa93ae73a7ca
Rodrigo Aguillar <flavioaguilar2001@gmail.com>
2025-08-06 13:44:25 -0300

    feat: Criando Card de planos

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
     <>
       <div className="bg-slate-600">
         Navbar
+        <Link to='/planos'> Planos</Link>
       </div>
     </>
   )
