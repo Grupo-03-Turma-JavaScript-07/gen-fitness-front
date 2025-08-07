f29233ca5ef22f967e3da85986a7a842bcb28f63
LuanaTechVet <luanaap2702@gmail.com>
2025-08-07 10:18:15 -0300

    fix: Contato

--- src/pages/contato/Contato.tsx
+++ src/pages/contato/Contato.tsx
@@ -8,58 +8,49 @@ function Contato() {
   return (
     <>
       <main className=" bg-black">
-        <section className="flex items-center justify-center gap-10 mx-auto bg-conic-120 from-blue-700 via-blue-400 to-black p-10 ">
-          <div className="w-full">
+        <div className="container flex flex-col mt-4 ">
+          <div className="grid md:grid-cols-2 m-1 gap-4 py-4">
+        <section className="grid gap-8  bg-conic-120 from-blue-700 via-blue-400 to-black rounded-2xl ml-2 p-10 ">
             <form className="flex flex-col p-5 rounded-2xl shadow-lg bg-white/5 w-full " onSubmit={msg}>
               <h2 className="text-2xl text-white font-bold text-shadow-sm text-center">
                 SAIBA MAIS</h2>
               <h3 className="font-medium text-white/90 text-center shadow-inner bg-black/5 p-2 my-1 mx-auto w-70% ">Conheça versões exclusivas!
               </h3>
-              <div className="grid md:grid-cols-2 m-1 gap-4 py-4">
-                <div className="flex flex-col-2 gap-2">
-                  <label htmlFor="nome" className="text-white font-bold text-shadow-sm">Nome:</label>
+                <div className="flex gap-2">
+                  <label htmlFor="nome" className="text-white font-bold text-shadow-sm p-1 my-2">Nome:</label>
                   <input
                     type="text"
                     placeholder="Digite seu nome"
                     name="nome"
                     required
-                    className="border-1 border-white rounded-3xl placeholder:text-[#ffffff80] p-1" />
+                    className="border-1 w-140 border-white rounded-3xl placeholder:text-[#ffffff80] p-1 my-2" />
                 </div>
-                <div className="flex flex-col-2 gap-2">
-                  <label htmlFor="telefone" className="text-white font-bold text-shadow-sm">Telefone:</label>
-                  <input
-                    type="text"
-                    placeholder="Digite seu telefone"
-                    name="telefone"
-                    className="border-1 border-white rounded-3xl placeholder:text-[#ffffff80] p-1" />
-                </div>
-              </div>
               <div className="flex flex-col gap-4">
                 <div className="flex flex-col-2 gap-2">
-                  <label htmlFor="email" className="text-white font-bold text-shadow-sm">E-mail:</label>
+                  <label htmlFor="email" className="text-white font-bold text-shadow-sm p-1 my-2">E-mail:</label>
                   <input
                     type="text"
                     placeholder="Digite seu e-mail"
                     name="email"
                     required
-                    className="border-1 border-white rounded-3xl placeholder:text-[#ffffff80] w-md p-1" />
+                    className="border-1 w-140 border-white rounded-3xl placeholder:text-[#ffffff80] p-1 my-2" />
                 </div>
                 <div className="flex flex-col-2 gap-2">
-                  <label htmlFor="assunto" className="text-white font-bold text-shadow-sm">Assunto:</label>
+                  <label htmlFor="assunto" className="text-white font-bold text-shadow-sm p-1 my-2">Assunto:</label>
                   <input
                     type="text"
                     placeholder="Sobre o que deseja falar?"
                     name="assunto"
                     required
-                    className="border-1 border-white rounded-3xl placeholder:text-[#ffffff80] w-md p-1" />
+                    className="border-1 w-136 border-white rounded-3xl placeholder:text-[#ffffff80] p-1 my-2" />
                 </div>
                 <div className="flex flex-col-2 gap-2">
-                  <label htmlFor="mensagem" className="text-white font-bold text-shadow-sm">Mensagem:</label>
+                  <label htmlFor="mensagem" className="text-white font-bold text-shadow-sm p-1 my-2">Mensagem:</label>
                   <textarea
                     placeholder="Digite a sua mensagem"
                     name="mensagem"
                     required
-                    className="border-1 border-white rounded-2xl placeholder:text-[#ffffff80] w-lg p-1" />
+                    className="border-1 w-130 border-white rounded-2xl placeholder:text-[#ffffff80] p-1 my-2" />
                 </div>
                 <button
                   type='submit'
@@ -67,24 +58,10 @@ function Contato() {
                 >Enviar
                 </button>
               </div>
-            </form>
-          </div>
-          
+            </form>              
         </section>
-        <section className="grid grid-cols-3 container mx-auto items-center justify-center gap-10 p-5">
-          <div className="bg-white rounded-2xl w-full h-full shadow-lg shadow-white/25 p-6">
-            <div className="flex gap-4 mb-2 border-b border-blue-500 pb-2">
-              <ChatsCircleIcon size={32} color="#2b7fff" weight="fill" />
-              <h2 className="text-2xl font-extrabold text-blue-500 ">CONTATOS</h2>
-            </div>
-            <h3 className="font-bold pt-1">TELEFONE:
-            </h3>
-            <p mb-4 text-sm text-black>+55 011 1234-5678</p>
-            <hr className="w-1/4 h-0.5 mx-auto mt-3 mb-2 bg-black/30 border-0" />
-            <h3 className="font-bold">E-MAIL:
-            </h3>
-            <p mb-4 text-sm text-black>grupogeneration03@gmail.com</p>
-          </div>
+        <section className="grid grid-cols-2 w-auto h-fit container mx-auto items-center justify-center gap-10 p-5">
+         
           <div className="bg-white rounded-2xl shadow-lg shadow-white/25 p-6">
             <div className="flex gap-4 mb-2 border-b border-blue-500 pb-2">
               <BuildingOfficeIcon size={32} color="#2b7fff" weight="fill" />
@@ -100,7 +77,18 @@ function Contato() {
             <h3 className="font-bold">FUNCIONAMENTO:
             </h3>
             <p mb-4 text-sm text-black>Seg a Sex: 08h às 17h</p>
-            <p>Online: 24h</p>
+            <p className="mb-10">Online: 24h</p>
+            <div className="flex gap-4 mb-2 border-b border-blue-500 pb-2">
+            <ChatsCircleIcon size={32} color="#2b7fff" weight="fill" />
+            <h2 className="text-2xl font-extrabold text-blue-500  justify-center ">CONTATOS</h2>
+            </div>
+            <h3 className="font-bold pt-1">TELEFONE:
+            </h3>
+            <p mb-4 text-sm text-black>+55 011 1234-5678</p>
+            <hr className="w-1/4 h-0.5 mx-auto mb-2 bg-black/30 border-0" />
+            <h3 className="font-bold">E-MAIL:
+            </h3>
+            <p mb-4 text-sm text-black>grupogeneration03@gmail.com</p>
           </div>
           <div className="overflow-hidden rounded-2xl shadow-lg shadow-white/25 h-full">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.1791706451368!2d-46.6847684!3d-23.56200769999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce579c21e26427%3A0x9aa89a73daf0d500!2sR.%20Teodoro%20Sampaio%2C%201629%20-%20Pinheiros%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2005415-030!5e0!3m2!1spt-BR!2sbr!4v1754530666421!5m2!1spt-BR!2sbr" 
@@ -109,7 +97,9 @@ function Contato() {
               loading="lazy" 
               referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
-        </section>
+            </section>
+        </div>
+        </div>
       </main>
     </>
   )
--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -1,4 +1,3 @@
-
 import CardSobre from "../../components/sobre/cardsobre/CardSobre";
 import { devs } from "../../data/devs";
 
@@ -7,20 +6,19 @@ function Sobre() {
 
   const equipe = devs
 
-  return (
-    <>
-      <h1 className="text-center text-white text-5xl p-4">Sobre nós</h1>
-      <div className="mt-auto flex justify-center w-full bg-black my-4 ">
-        <div className="container w-[70vw] flex flex-col mt-4 ">
-          <div className="grid grid-cols-1 grid-row: auto md:grid-cols-3 lg:grid-cols-4 gap-8">
-            {equipe.map((sobre) => (
-                <CardSobre key={sobre.id} devs={sobre} />
-            ))}
-          </div>
-        </div>
-      </div>
-    </>
-  )
+ return (
+        <>
+            <div className="mt-auto flex justify-center w-full bg-black my-4 ">
+                <div className="container flex flex-col mt-4 ">
+                    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+                       {equipe.map((sobre) => (
+                            <CardSobre key={sobre.id} devs={sobre} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Sobre;
