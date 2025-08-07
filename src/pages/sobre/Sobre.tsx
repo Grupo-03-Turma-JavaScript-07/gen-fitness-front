7d6bb3b3f27808400cf08792f663b92882cb9569
Rodrigo Aguillar <flavioaguilar2001@gmail.com>
2025-08-07 13:19:08 -0300

    feat: Ajustando o sobre

--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -6,38 +6,50 @@ function Sobre() {
 
   return (
     <>
-      <h1 className="text-center text-white text-5xl p-4">Sobre nós</h1>
+      <section className="w-full min-h-screen flex justify-center items-center bg-[url('https://midias.siteware.com.br/wp-content/uploads/2021/02/25044214/exemplo-missao-de-empresas-1.jpg')] bg-fixed bg-cover bg-center">
+      
+        <div className="bg-black bg-opacity-60 w-full md:w-[80vw] p-8 md:p-12 text-white rounded-lg shadow-lg">
+          <div className="mb-8">
+            <h2 className="text-3xl font-extrabold mb-2">Missão</h2>
+            <p className="text-lg">
+              Desenvolver soluções tecnológicas que tornem a rotina de personal trainers e academias mais prática, organizada e eficiente, facilitando o acompanhamento e o crescimento profissional no universo fitness.
+            </p>
+          </div>
 
-      <div className="container w-[80vw] mx-auto text-center">
-        <div className="text-white">
-          <h2 className="text-2xl">Missão</h2>
-          <p>Desenvolver soluções tecnológicas que tornem a rotina de personal trainers e academias mais prática, organizada e eficiente, facilitando o acompanhamento e o crescimento profissional no universo fitness.</p>
+          <div className="mb-8">
+            <h2 className="text-3xl font-extrabold mb-2">Visão</h2>
+            <p className="text-lg">
+              Ser reconhecida como uma plataforma referência no apoio digital para profissionais de educação física, unindo tecnologia acessível, personalização e melhoria contínua da experiência de atendimento ao aluno.
+            </p>
+          </div>
 
-          <h2>Visão</h2>
-          <p>Ser reconhecida como uma plataforma referência no apoio digital para profissionais de educação física, unindo tecnologia acessível, personalização e melhoria contínua da experiência de atendimento ao aluno.</p>
-
-          <h2>Valores</h2>
-          <p>Colaboração e aprendizado: Nascemos como um projeto de desenvolvimento web, e seguimos evoluindo com base no aprendizado constante.
-            Inovação com propósito: Criamos para resolver problemas reais do dia a dia fitness.
-            Praticidade e eficiência: Priorizamos soluções simples, rápidas e eficazes.
-            Valorização do profissional fitness: Nosso foco é apoiar quem promove saúde, bem-estar e transformação de vidas.
-          </p>
+          <div>
+            <h2 className="text-3xl font-extrabold mb-2">Valores</h2>
+            <p className="text-lg">
+              <strong>Colaboração e aprendizado:</strong> Nascemos como um projeto de desenvolvimento web, e seguimos evoluindo com base no aprendizado constante. <br />
+              <strong>Inovação com propósito:</strong> Criamos para resolver problemas reais do dia a dia fitness. <br />
+              <strong>Praticidade e eficiência:</strong> Priorizamos soluções simples, rápidas e eficazes. <br />
+              <strong>Valorização do profissional fitness:</strong> Nosso foco é apoiar quem promove saúde, bem-estar e transformação de vidas.
+            </p>
+          </div>
         </div>
+      </section>
 
-        <div className="mt-auto flex justify-center w-full bg-black my-4 ">
-          <div className="container w-[80vw] flex flex-col mt-4">
-            <div className="flex flex-wrap flex-row justify-center gap-4 ">
-              {equipe.map((sobre) => (
-                <CardSobre key={sobre.id} devs={sobre} />
-              ))}
-            </div>
+      <div className="mt-auto flex justify-center w-full my-4">
+        <div className="container w-[80vw] flex flex-col mt-4">
+          <div className="flex flex-wrap flex-row justify-center gap-4">
+            {equipe.map((sobre) => (
+              <CardSobre key={sobre.id} devs={sobre} />
+            ))}
           </div>
         </div>
       </div>
+
     </>
   );
 }
 
 export default Sobre;
+
 
 
